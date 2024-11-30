5a73e24b3a07a91813a2a68a136da5d0c5b49eba
Ogun Akar <ogunakar09@gmail.com>
2024-11-30 20:30:04 +0300

    feat: project init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Movie Viewer
+
+## :battery: Features
+
+This repository comes 🔋 packed with:
+
+- **React.js**;
+- **TypeScript**;
+- **Shadcn/ui**;
+- **TailwindCSS**;
+- **SASS** & **SCSS**;
+- **ESLint**;
+- **Prettier**;
+- **Husky**;
+- **Commitlint**;
+- **Lint staged**;
+- **Playwright**;
+- **Vite**;
---
+++ _config.ts
@@ -0,0 +1,15 @@
+const config = {
+  metadata: {
+    title: 'Vite React.js TypeScript & Shadcn/ui — Template',
+    description:
+      'A React.js Web App, SPA, website or landing page with TypeScript, Shadcn/ui, TailwindCSS, Vite, SWC, ESLint, Husky ',
+    keywords:
+      'react.js, react.js-template, react.js-boilerplate, typescript, shadcn/ui, tailwindcss, vite, swc, eslint, prettier, husky, commitlint, lint-staged'
+  },
+  server: {
+    host: 'localhost',
+    port: 3000
+  }
+};
+
+export default config;
