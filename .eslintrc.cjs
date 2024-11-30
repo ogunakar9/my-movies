f38c250298be4e0e52d07905bb021a6fc325211a
Ogun Akar <ogunakar09@gmail.com>
2024-11-30 20:34:15 +0300

    refactor: eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,65 +13,44 @@ module.exports = {
     'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:@typescript-eslint/recommended-type-checked'
   ],
-
   parser: '@typescript-eslint/parser',
-
   parserOptions: {
     project: true,
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-
   settings: {
     react: {
       pragma: 'React',
-
       version: 'detect'
     }
   },
-
   ignorePatterns: ['**/*.html'],
-
   rules: {
     // base
     indent: ['error', 2, { SwitchCase: 1 }],
-
     'linebreak-style': ['error', 'unix'],
-
     quotes: ['error', 'single'],
-
     semi: ['error', 'always'],
-
     // end
-
     // typescript
-
     '@typescript-eslint/array-type': 'off',
-
     '@typescript-eslint/consistent-type-definitions': 'off',
-
     '@typescript-eslint/consistent-type-imports': [
       'warn',
-
       {
         prefer: 'type-imports',
-
         fixStyle: 'inline-type-imports'
       }
     ],
-
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-
     '@typescript-eslint/require-await': 'off',
-
     '@typescript-eslint/no-misused-promises': [
       'error',
-
       {
         checksVoidReturn: { attributes: false }
       }
     ]
-
     // end
   }
 };
